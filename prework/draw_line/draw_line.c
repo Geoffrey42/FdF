eb1ce9a64eec62a9655e4cc61d98233632b3fbe4
Geoffrey GANE <ggane@e1r5p6.42.fr>
2017-09-21 17:26:28 +0200

    ajout initialize_graph()

--- prework/draw_line/draw_line.c
+++ prework/draw_line/draw_line.c
@@ -18,6 +18,16 @@ void	draw_line(t_graph *graph)
 	mlx_pixel_put(graph->mlx, graph->win, x, y, 0x00FFFFFF);
 }
 
+void	initialize_graph(t_graph **graph, void *mlx, void *win)
+{
+	(*graph)->mlx = mlx;
+	(*graph)->win = win;
+	(*graph)->spot_1[0] = 3;
+	(*graph)->spot_1[1] = 9;
+	(*graph)->spot_2[0] = 9;
+	(*graph)->spot_2[1] = 14;
+}
+
 int		main(void)
 {
 	void	*mlx;
@@ -28,12 +38,8 @@ int		main(void)
 	win = mlx_new_window(mlx, 1000, 1000, "draw_line");
 	if (!(graph = (t_graph *)malloc(sizeof(t_graph))))
 		return (-1);
-	graph->mlx = mlx;
-	graph->win = win;
-	graph->spot_1[0] = 3;
-	graph->spot_1[1] = 9;
-	graph->spot_2[0] = 9;
-	graph->spot_2[1] = 14;
+	initialize_graph(&graph, mlx, win);
+	draw_line(graph);
 	mlx_loop(mlx);
 	return (0);
 }
