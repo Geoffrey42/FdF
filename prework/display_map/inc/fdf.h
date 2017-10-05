db1b5bb080e7d5219425c22b374fd2afdd71423e
Geoffrey42 <geoffreygane@gmail.com>
2017-10-05 15:19:47 +0200

    creation de inc/ mais non tested, il y aura surement des problemes avec les includes du coup

---
+++ prework/display_map/inc/fdf.h
@@ -0,0 +1,20 @@
+#ifndef FDF_H
+# define FDF_H
+
+# include "mlx.h"
+# include "libft.h"
+
+typedef struct  s_3d
+{
+    int         x;
+    int         y;
+    int         z;
+}               t_3d;
+
+typedef struct  s_2d;
+{
+    int         x;
+    int         y;
+}               t_2d;
+
+#endif
---
+++ prework/display_map/main.c
@@ -0,0 +1,18 @@
+#include "fdf.h"
+#include <stdio.h>
+#include <math.h>
+
+int		main(int ac, char **av)
+{
+    t_3d    raw_coordinates;
+    t_2d    converted_coordinates;
+    void    *image_id;
+
+    if (is_not_a_correct_map(ac, av))
+        return (-1);
+    raw_coordinates = get_map_coordinates(av[1]);
+    converted_coordinates = convert_3d_coordinates_for_isometric_projection(raw_coordinates);
+    image_id = get_image_from(converted_coordinates);
+    display_image_in_window(image_id);
+	return (0);
+}
