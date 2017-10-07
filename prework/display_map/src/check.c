60e6e50c10cc3e204fbfbc245424010955115de3
Geoffrey42 <geoffreygane@gmail.com>
2017-10-08 00:33:15 +0200

    test is_invalid_lines() ok

--- prework/display_map/src/check.c
+++ prework/display_map/src/check.c
@@ -7,6 +7,42 @@
 ** 3- le contenu est une map valide
 */
 
+static int  get_spaces_nb(char *line)
+{
+    int     i;
+    int     space_nb;
+
+    i = 0;
+    while (line[i])
+    {
+        if (line[i] == ' ' && line[i - 1] != ' ' && i != 0)
+            space_nb++;
+        i++;
+    }
+    return (space_nb);
+}
+
+static int  is_invalid_lines(char *map_name)
+{
+    int     fd;
+    int     spaces_nb;
+    char    *line;
+
+    fd = get_map_fd(map_name);
+    get_next_line(fd, &line);
+    spaces_nb = get_spaces_nb(line);
+    while (get_next_line(fd, &line) != 0)
+    {
+        if (get_spaces_nb(line) != spaces_nb)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+    close(fd);
+    return (0);
+}
+
 static int  is_not_fdf_extension(char *map_name)
 {
     char    *extension;
@@ -42,7 +78,7 @@ static int  is_not_valid_format(char *map_name)
 {
     char    *map_copy;
 
-    if (is_not_fdf_extension(map_name);
+    if (is_not_fdf_extension(map_name)
         return (-1);
     map_copy = copy_map_in_a_string(map_name);
     if (is_invalid_characters(map_copy))
