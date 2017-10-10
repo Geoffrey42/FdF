81a87faaf288fe6368b87a561ea681e79f56278f
Geoffrey42 <geoffreygane@gmail.com>
2017-10-11 00:49:35 +0200

    ajout de trucs dans 3d_map_coordinates.c mais non tested et surement pas operationnel.

--- src/3d_map_coordinates.c
+++ src/3d_map_coordinates.c
@@ -14,9 +14,39 @@
 
 t_3d    get_map_coordinates(char *map_name)
 {
-    t_3d    *raw_coordinates;
+    t_3d    **coord;
+    int     i;
+    int     j;
+    int     line;
+    int     column;
 
-    (void)map_name;
-    raw_coordinates = (t_3d *)malloc(sizeof(t_3d));
-    return (*raw_coordinates);
+    i = 0;
+    j = 0;
+    line = 0;
+    column = 0;
+    if (!(coord = (t_3d **)malloc(sizeof(get_coord_nb(map_name)))))
+        return (NULL);
+    while (map[i])
+    {
+        if (ft_isdigit(map[i]))
+        {
+            coord[j]->x = column;
+            coord[j]->y = line;
+            if (is_more_than_single_digit(map, i))
+            {
+               coord[j]->z = get_whole_number(map, i); 
+               i = go_to_last_digit(map, i);
+            }
+            else
+                coord[j]->z = ft_atoi(map[i]);
+            j++;
+        }
+        else if (map[i] == '\n')
+        {
+            column = 0;
+            line++;
+        }
+        i++;
+    }
+    return (*coord);
 }
