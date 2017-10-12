41b948f3f688ec7d18eac8ccd0faa190a5780f1e
Geoffrey42 <geoffreygane@gmail.com>
2017-10-12 19:46:47 +0200

    je stop la mise a la norme des fichiers de tests/ car ils ne sont pas a rendre au final

--- tests/test_00/copy_map_in_a_string.c
+++ tests/test_00/copy_map_in_a_string.c
@@ -1,55 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   copy_map_in_a_string.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/12 19:42:11 by ggane             #+#    #+#             */
+/*   Updated: 2017/10/12 19:44:04 by ggane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
-#include <stdio.h>
 
-static size_t   get_map_file_size(int fd)
+static size_t	get_map_file_size(int fd)
 {
-    size_t  size;
-    char    buf[MAP_BUF_SIZE];
+	size_t	size;
+	char	buf[MAP_BUF_SIZE];
 
-    size = 0;
-    while (read(fd, buf, MAP_BUF_SIZE))
-        size++;
-    return (size);
+	size = 0;
+	while (read(fd, buf, MAP_BUF_SIZE))
+		size++;
+	return (size);
 }
 
-static void     copy_file_content(int fd, char **map_copy)
+static void		copy_file_content(int fd, char **map_copy)
 {
-    char    buf[MAP_BUF_SIZE];
-    char    *tmp;
-    int     i;
-    int     ret;
+	char	buf[MAP_BUF_SIZE];
+	char	*tmp;
+	int		i;
+	int		ret;
 
-    i = 0;
-    tmp = *map_copy;
-    while (read(fd, buf, MAP_BUF_SIZE))
-        tmp[i++] = buf[0];
+	i = 0;
+	tmp = *map_copy;
+	while (read(fd, buf, MAP_BUF_SIZE))
+		tmp[i++] = buf[0];
 }
 
-char     *copy_map_in_a_string(char *map_name)
+char			*copy_map_in_a_string(char *map_name)
 {
-    size_t  size;
-    char    *map_copy;
-    int     fd;
+	size_t	size;
+	char	*map_copy;
+	int		fd;
 
-    fd = get_map_fd(map_name);
-    size = get_map_file_size(fd);
-    close(fd);
-    fd = get_map_fd(map_name);
-    if (!(map_copy = (char *)malloc(sizeof(char) * size + 1)))
-        return (NULL);
-    map_copy[size] = '\0';
-    copy_file_content(fd, &map_copy);
-    close(fd);
-    return (map_copy);
+	fd = get_map_fd(map_name);
+	size = get_map_file_size(fd);
+	close(fd);
+	fd = get_map_fd(map_name);
+	if (!(map_copy = (char *)malloc(sizeof(char) * size + 1)))
+		return (NULL);
+	map_copy[size] = '\0';
+	copy_file_content(fd, &map_copy);
+	close(fd);
+	return (map_copy);
 }
 
-int             get_map_fd(char *map_name)
+int				get_map_fd(char *map_name)
 {
-    int     fd;
+	int		fd;
 
-    fd = open(map_name, O_RDONLY);
-    if (fd == -1)
-        exit(print_error_msg("open() failed"));
-    else
-        return (fd);
+	fd = open(map_name, O_RDONLY);
+	if (fd == -1)
+		exit(print_error_msg("open() failed"));
+	else
+		return (fd);
 }
--- tests/test_00/errors.c
+++ tests/test_00/errors.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/12 19:44:34 by ggane             #+#    #+#             */
+/*   Updated: 2017/10/12 19:44:41 by ggane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-int     print_error_msg(char *error_msg)
+int		print_error_msg(char *error_msg)
 {
-    ft_putendl_fd(error_msg, 2);
-    return (-1);
+	ft_putendl_fd(error_msg, 2);
+	return (-1);
 }
