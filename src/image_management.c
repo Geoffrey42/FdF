ab0f67dae90c1d2aeaf354930daf594960f03135
Geoffrey42 <geoffreygane@gmail.com>
2017-10-15 00:29:45 +0200

    remplissage image en cours

--- src/image_management.c
+++ src/image_management.c
@@ -12,21 +12,30 @@
 
 #include "fdf.h"
 
-static t_image  *create_image(t_2d *coordinates)
+static t_image  *create_image(t_2d *coordinates, t_mlx *data)
 {
-    t_image     *image;
+	t_image		*image;
 
-    if (!(image = (t_image *)malloc(sizeof(t_image))))
-        return (NULL);
-    image->id = mlx_new_image();
-    return (image);
+	if (!(image = (t_image *)malloc(sizeof(t_image))))
+		return (NULL);
+	image->id = mlx_new_image(data->mlx);
+	return (image);
+}
+
+static void		fill_image(t_image *image, t_2d *coordinates, t_mlx *data)
+{
+	int		bpp;
+	int		size_line;
+	int		endian;
+
+	data->str = mlx_get_data_addr(image->id, &bpp, &size_line, &endian);
 }
 
 void	        *get_image_from(t_2d **converted_coordinates, t_mlx *data)
 {
-	t_image     *image;
+	t_image		*image;
 
-	image = create_image(converted_coordinates);
-    fill_image(image, converted_coordinates, data);
+	image = create_image(converted_coordinates, data);
+	fill_image(image, converted_coordinates, data);
 	return (image);
 }
