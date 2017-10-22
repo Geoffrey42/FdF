5940526bc02af44d5d0b44863c67db24e810348d
Geoffrey42 <geoffreygane@gmail.com>
2017-10-22 22:01:32 +0200

    dessin des traits ok mais resultat peu satisfaisant

--- src/draw_directly_in_window.c
+++ src/draw_directly_in_window.c
@@ -6,7 +6,7 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/22 19:48:40 by ggane             #+#    #+#             */
-/*   Updated: 2017/10/22 21:48:28 by ggane            ###   ########.fr       */
+/*   Updated: 2017/10/22 21:57:36 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,21 @@ void		draw_directly_in_window(t_data *data)
 
 	x = 0;
 	y = 0;
-	ft_putnbrdl(data->x_max);
-	ft_putnbrdl(data->y_max);
 	while (y < data->y_max)
 	{
 		while (x < data->x_max)
 		{
 			z = data->coordinates[y][x];
 			x_2d = get_x_for_isometric_projection(x, y);
-			ft_putendl("get_x_for_isometric_projection() for x ok");
 			y_2d = get_y_for_isometric_projection(x, y, z);
-			ft_putendl("get_y_for_isometric_projection() for y ok");
 			if (x + 1 < data->x_max && y + 1 < data->y_max)
 			{
 				next_z = data->coordinates[y + 1][x + 1];
 				next_x_2d = get_x_for_isometric_projection(x + 1, y + 1);
-				ft_putendl("get_x_for_isometric_projection() for x + 1 ok");
 				next_y_2d = get_y_for_isometric_projection(x + 1, y + 1, next_z);
-				ft_putendl("get_y_for_isometric_projection() for y + 1 ok");
 			}
-			mlx_pixel_put(data->mlx, data->win, x_2d + 50, y_2d + 50, WHITE);
-			ft_putendl("mlx_pixel_put() ok");
-			//draw_line(x, y, x + 1, y + 1, data);
+			//mlx_pixel_put(data->mlx, data->win, x_2d + 50, y_2d + 50, WHITE);
+			draw_line(x_2d + 75, y_2d + 75, next_x_2d + 75, next_y_2d + 75, data);
 			x++;
 		}
 		x = 0;
--- src/draw_line.c
+++ src/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/22 19:52:58 by ggane             #+#    #+#             */
-/*   Updated: 2017/10/22 20:19:13 by ggane            ###   ########.fr       */
+/*   Updated: 2017/10/22 21:56:23 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,17 @@ void		draw_line(int x1, int y1, int x2, int y2, t_data *data)
 	int		coef_directeur;
 	int		ordonnee_a_lorigine;
 
-	xn = 0;
+	xn = x1;
 	if (x1 == x2)
     {
         yn = y1;
         if (y1 > y2)
 		{
+			swap = y2;
 			yn = y2;
 			xn = x2;
 			y2 = y1;
-			y1 = yn;
+			y1 = swap;
 		}
 		while (yn <= y2)
 			mlx_pixel_put(data->mlx, data->win, xn++, yn++, RED);
@@ -37,12 +38,13 @@ void		draw_line(int x1, int y1, int x2, int y2, t_data *data)
 	}
 	if (x1 > x2)
 	{
+		swap = x1;
 		x1 = x2;
 		xn = x1;
-		x2 = xn;
+		x2 = swap;
 		swap = y1;
 		y1 = y2;
-		y2 = y1;
+		y2 = swap;
 	}
 	coef_directeur = (y2 - y1) / (x2 - x1);
 	ordonnee_a_lorigine = y1 - coef_directeur * x1;
