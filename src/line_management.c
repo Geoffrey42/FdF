7c48627f09133407a6f21f589289160925974a60
Geoffrey42 <ggane@student.42.fr>
2017-11-17 00:24:36 +0100

    suppression fonction inutile dans src/line_management.c

--- src/line_management.c
+++ src/line_management.c
@@ -6,7 +6,7 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 19:21:13 by ggane             #+#    #+#             */
-/*   Updated: 2017/11/17 00:18:53 by ggane            ###   ########.fr       */
+/*   Updated: 2017/11/17 00:23:58 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,13 +128,6 @@ static int		need_several_x_for_one_y(t_dot *p1, t_dot *p2)
 	return (0);
 }
  
-static int		need_several_y_for_one_x(t_dot *p1, t_dot *p2)
-{
-	if ((p2->x - p1->x) < abs(p2->y - p1->y))
-		return (1);
-	return (0);
-}
- 
 void			draw_one_line(t_data *data, t_dot *p1, t_dot *p2)
 {
 	if (need_several_x_for_one_y(p1, p2))
@@ -142,7 +135,7 @@ void			draw_one_line(t_data *data, t_dot *p1, t_dot *p2)
 		data->draw_corresponding_line = &draw_one_almost_horizontal_line;
 		choose_line_angle_and_draw_it(data, p1, p2);
 	}
-	else if (need_several_y_for_one_x(p1, p2))
+	else
 	{
 		data->draw_corresponding_line = &draw_one_almost_vertical_line;
 		choose_line_angle_and_draw_it(data, p1, p2);
